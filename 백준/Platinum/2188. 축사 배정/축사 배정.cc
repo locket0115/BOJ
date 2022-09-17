4d005634aed76f49c0c90f4db4a0c0791b0b43e1
locket0115 <66500458+locket0115@users.noreply.github.com>
2022-09-17 11:18:58 +0900

    [Platinum IV] Title: 축사 배정, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Platinum/2188. 축사 배정/축사 배정.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 202
+
+vector<int> vtx[MAX];
+int slt[MAX];
+bool done[MAX];
+int N, M;
+
+bool dfs(int x) {
+	for (int i = 0; i < vtx[x].size(); i++) {
+		int p = vtx[x][i];
+		if (done[p]) continue;
+		done[p] = true;
+		
+        if (slt[p] == 0 || dfs(slt[p])) {
+			slt[p] = x;
+			return true;
+		}
+		
+	}
+	return false;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> N >> M;
+
+    for(int i = 1; i <= N; i++) {
+        int s, t; cin >> s;
+
+        for(int j = 0; j < s; j++) {
+            cin >> t;
+            vtx[i].push_back(t);
+        }
+    }
+
+    int cnt = 0;
+	for (int i = 1; i <= N; i++) {
+		fill(done, done + MAX, false);
+		if (dfs(i)) cnt++;
+	}
+
+    cout << cnt;
+}
